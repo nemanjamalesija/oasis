0454ad1eeace3c436b5dee5cfaecbfe94da178bc
nemanjamalesija <malesija13@gmail.com>
2023-10-10 20:08:59 +0200

    Connect supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://rzakgratzmyihtuhgnmw.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6YWtncmF0em15aWh0dWhnbm13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY4OTUwODYsImV4cCI6MjAxMjQ3MTA4Nn0.juwhFy_TuxU82bEI7SiWeu9UeS7i2vmm18BSvMpE8Qw';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
