cb00fe55b2db363f4cdd566089ee0cc4d37b0ce5
nemanjamalesija <malesija13@gmail.com>
2023-10-31 22:11:56 +0100

    Add more precise positioning of the popup modal

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -68,21 +68,34 @@ const MenusContext = createContext();
 
 function Menus({ children }) {
   const [openId, setOpenId] = useState();
+  const [position, setPosition] = useState(null);
 
   const close = () => setOpenId('');
   const open = setOpenId;
 
   return (
-    <MenusContext.Provider value={{ openId, close, open }}>
+    <MenusContext.Provider
+      value={{ openId, close, open, position, setPosition }}
+    >
       {children}
     </MenusContext.Provider>
   );
 }
 
 function Toggle({ id }) {
-  const { openId, close, open } = useContext(MenusContext);
+  const { openId, close, open, setPosition } =
+    useContext(MenusContext);
 
-  function handleClick() {
+  function handleClick(e) {
+    const rect = e.target
+      .closest('button')
+      .getBoundingClientRect();
+
+    setPosition({
+      x: window.innerWidth - rect.width - rect.x,
+      y: rect.y + rect.height + 8,
+    });
+
     openId === '' || openId !== id ? open(id) : close();
   }
 
@@ -94,14 +107,12 @@ function Toggle({ id }) {
 }
 
 function List({ id, children }) {
-  const { openId } = useContext(MenusContext);
+  const { openId, position } = useContext(MenusContext);
 
   if (openId !== id) return null;
 
   return createPortal(
-    <StyledList position={{ x: 20, y: 20 }}>
-      {children}
-    </StyledList>,
+    <StyledList position={position}>{children}</StyledList>,
     document.body
   );
 }
