4fded36c1989aa6e866abb4ab59cc1f5aca76c71
nemanjamalesija <malesija13@gmail.com>
2023-11-03 00:04:28 +0100

    Load mock data from the database

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3,7 +3,10 @@ import { differenceInDays } from 'date-fns/esm';
 
 // We want to make this function work for both Date objects and strings (which come from Supabase)
 export const subtractDates = (dateStr1, dateStr2) =>
-  differenceInDays(parseISO(String(dateStr1)), parseISO(String(dateStr2)));
+  differenceInDays(
+    parseISO(String(dateStr1)),
+    parseISO(String(dateStr2))
+  );
 
 export const formatDistanceFromNow = (dateStr) =>
   formatDistance(parseISO(dateStr), new Date(), {
@@ -25,6 +28,7 @@ export const getToday = function (options = {}) {
 };
 
 export const formatCurrency = (value) =>
-  new Intl.NumberFormat('en', { style: 'currency', currency: 'USD' }).format(
-    value
-  );
+  new Intl.NumberFormat('en', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(value);
