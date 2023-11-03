2a0d08a0e93012beb0feec8b62fffc74bae25c98
nemanjamalesija <malesija13@gmail.com>
2023-11-03 01:07:37 +0100

    Add server side filtering

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,10 +1,20 @@
 import { useQuery } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBookings';
+import { useSearchParams } from 'react-router-dom';
 
 export default function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParams.get('status');
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'status', value: filterValue };
+
   const { isLoading, data: bookings } = useQuery({
     queryKey: ['bookings'],
-    queryFn: getBookings,
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isLoading, bookings };
